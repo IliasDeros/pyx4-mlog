0cc26cdaaa4600ae6e698c26a431a6e25e3ab2b6
Ilias <ideros21@gmail.com>
2019-06-06 15:00:01 -0400

    Initialize basic authentication

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  
+  post 'login', to: 'sessions#create', as: 'login'
+  post 'logout', to: 'sessions#destroy', as: 'logout'
 end
